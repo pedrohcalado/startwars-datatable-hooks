c99a5d00495252baf9ca45bddb22f0b6bf7c39d9
pedrohcalado <pedrocalado22@gmail.com>
2020-10-28 19:22:07 -0300

    Add loading component when the API is being called

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import fetchAPI from '../../services/swapiAPI';
 import { StarWarsContext } from '../../context/StarWarsContext';
 import TableHeader from './TableHeader';
@@ -7,15 +7,17 @@ import NameFilter from '../Filters/NameFilter';
 import NumericFilter from '../Filters/NumericFilter';
 import DeleteNumericFilter from '../Filters/DeleteNumericFilter';
 import Order from '../Filters/Order';
+import Loading from '../Loading/Loading';
 
 export default function Table() {
-
   const { sets: { setData } } = useContext(StarWarsContext);
-
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const response = await fetchAPI().then((data) => data.results);
       setData(response);
+      setIsLoading(false);
     }
     fetchData();
   }, [setData]);
@@ -26,10 +28,11 @@ export default function Table() {
       <NumericFilter />
       <DeleteNumericFilter />
       <Order />
-      <table>
+      {isLoading && <Loading />}
+      {!isLoading && <table>
         <TableHeader />
         <TableBody />
-      </table>
+      </table>}
     </div>
   );
 }
